948a073356a6c18029a5f876f2cf8d071771622e
Mathijs-04 <mathijsvandermeijde@gmail.com>
2026-01-10 13:45:22 +0100

    Implemented product props

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -3,18 +3,41 @@ import Card from 'primevue/card'
 import DetailsButton from './DetailsButton.vue'
 import CartButton from './CartButton.vue'
 import PlaceholderImage from '../assets/Placeholder.png'
+
+const props = defineProps({
+    name: {
+        type: String,
+        default: 'Product'
+    },
+    description: {
+        type: String,
+        default: 'Description placeholder text'
+    },
+    price: {
+        type: Number,
+        default: 100
+    },
+    inStock: {
+        type: Boolean,
+        default: true
+    },
+    image: {
+        type: String,
+        default: '../src/assets/Placeholder.png'
+    }
+})
 </script>
 
 <template>
   <Card style="width: 25rem; overflow: hidden">
     <template #header>
-      <img alt="product image" :src="PlaceholderImage" style="width: 100%; height: auto; display: block;" />
+      <img alt="product image" :src="image" style="width: 100%; height: auto; display: block;" />
     </template>
-    <template #title>Product Name</template>
-    <template #subtitle>$99.99</template>
+    <template #title>{{ name }}</template>
+    <template #subtitle>{{ Price }} Gold</template>
     <template #content>
       <p class="m-0">
-        Product description placeholder text
+       {{ description }}
       </p>
     </template>
     <template #footer>
