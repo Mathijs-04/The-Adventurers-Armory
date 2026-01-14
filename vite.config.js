364e23abbedb4af158b98fe2790fa0e4b069a6d8
Mathijs-04 <mathijsvandermeijde@gmail.com>
2026-01-14 16:04:40 +0100

    Updated for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production'
+    ? '/The-Adventurers-Armory/'
+    : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
